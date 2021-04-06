70915d46f8aad1d9e95ceaefeaa08e4ddb630efa
Edney Paulo Gonçalves <desenvolvimento@cryptos.eti.br>
2021-04-06 15:15:04 -0300

    admin chad

--- resources/views/admin-side/viagens/viagem-show.blade.php
+++ resources/views/admin-side/viagens/viagem-show.blade.php
@@ -11,7 +11,7 @@
 <div class="row">
     <div class="col-12">
         <div class="card">
-            <div class="card-header">
+            <div class="header-vendas-viagem card-header">
                 <h3 class="card-title"></h3>
 
                 <div class="card-tools">
@@ -31,53 +31,54 @@
             <div class="row mt-4 p-1">
 
 
+            @for ($i = 0; $i < 5; $i++)
+
 
 
             @foreach ($viagem->vendas as $venda)
-            <!-- .card-venda -->
-            <div class="col-lg-4 m-auto">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between bg-secondary">
-                    <img src="{{ $venda->cliente->foto }}" class="img-circle img-size-32 mr-2">
-                    <h3 class="card-title">{{ $venda->cliente->name }}</h3>
-                </div>
-                <div class="card d-flex">
-                    <h3 class="card-title">Jardim Ype</h3>
-                    <a href="" target="_blank" rel="noopener noreferrer">Maps</a>
+            <div class=".card-ficha-venda col-lg-4">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between bg-secondary">
+                        <img src="{{ $venda->cliente->foto }}" class="img-circle img-size-32 mr-2">
+                        <h3 class="card-title">{{ $venda->cliente->name }}</h3>
+                    </div>
+                    <div class="d-flex">
+                        <h3 class="card-title">Jardim Ype</h3>
+                        <a href="" target="_blank" rel="noopener noreferrer">Maps</a>
 
-                </div>
+                    </div>
 
-                <div class="card-body table-responsive">
-                    <table class="table table-striped table-valign-middle">
-                        <thead>
-                            <tr>
-                                <th>Item</th>
-                                {{-- <th>Valor Total</th> --}}
-                                <th>Valor Unitário</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($venda->encomendas as $encomenda)
-                            <tr>
-                                <td>
-                                    <img src="{{ $encomenda->produto->foto }}" alt="Product 1"
-                                        class="img-circle img-size-32 mr-2">
-                                    {{ $encomenda->produto->name }}
-                                </td>
-                                <td>
-                                    $ {{ $encomenda->venda }}
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                    <div class="card-body table-responsive">
+                        <table class="table table-striped table-valign-middle">
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    {{-- <th>Valor Total</th> --}}
+                                    <th>Valor Unitário</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($venda->encomendas as $encomenda)
+                                <tr>
+                                    <td>
+                                        <img src="{{ $encomenda->produto->foto }}" alt="Product 1"
+                                            class="img-circle img-size-32 mr-2">
+                                        {{ $encomenda->produto->name }}
+                                    </td>
+                                    <td>
+                                        $ {{ $encomenda->venda }}
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
-            <!-- /.card-venda -->
             </div>
             @endforeach
 
 
+            @endfor
 
             </div>
 
